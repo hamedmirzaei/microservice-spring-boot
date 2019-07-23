802e16478e8643005deabe4722d1986bf5ef8415
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-07-23 17:26:13 +0430

    first commit: eureka-server on port 8761

---
+++ src/main/java/hm/microservice/spring/boot/MicroserviceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package hm.microservice.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceSpringBootApplication.class, args);
+    }
+
+}
